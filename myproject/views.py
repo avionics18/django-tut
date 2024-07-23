acedbd7a1c25092400ad5ab6a36a26a9fe653cea
avionics18 <avimurmu18@gmail.com>
2024-07-24 00:13:42 +0530

    Django app ready for deploy

---
+++ myproject/views.py
@@ -0,0 +1,10 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+def homepage(request):
+    # return HttpResponse("Welcome to the homepage.")
+    return render(request, "index.html")
+
+def about(request):
+    # return HttpResponse("This is the about page.")
+    return render(request, "about.html")
